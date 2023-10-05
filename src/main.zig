5f8ed6203a72bcfe7df6956259f9ce69d39084cf
dymaxion1934 <dymaxion@d.com>
2023-10-05 18:11:51 -0300

    abstract get_public_key() function.

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,8 @@
-const c = @cImport(@cInclude("secp256k1.h"));
 const std = @import("std");
+const keys = @import("keys.zig");
+
 const sha256 = std.crypto.hash.sha2.Sha256;
 const Check = std.heap.Check;
-
-const FLAGS_TYPE_CONTEXT = 1 << 0;
-const FLAGS_BIT_CONTEXT_VERIFY = 1 << 8;
-const FLAGS_BIT_CONTEXT_SIGN = 1 << 9;
-const FLAGS_TYPE_COMPRESSION = 1 << 1;
-const FLAGS_BIT_COMPRESSION = 1 << 8;
-
-const CONTEXT_VERIFY = (FLAGS_TYPE_CONTEXT | FLAGS_BIT_CONTEXT_VERIFY);
-const CONTEXT_SIGN = (FLAGS_TYPE_CONTEXT | FLAGS_BIT_CONTEXT_SIGN);
-const EC_COMPRESSED = (FLAGS_TYPE_COMPRESSION | FLAGS_BIT_COMPRESSION);
-const EC_UNCOMPRESSED = (FLAGS_TYPE_COMPRESSION);
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -32,18 +22,7 @@ pub fn main() !void {
     sha256.hash(&input, &sk, .{});
     std.debug.print("private key: {s}\n", .{std.fmt.fmtSliceHexLower(&sk)});
 
-    var c_pk: c.secp256k1_pubkey = undefined;
-    var ctx = c.secp256k1_context_create(CONTEXT_SIGN | CONTEXT_VERIFY);
-    if (c.secp256k1_ec_pubkey_create(ctx, &c_pk, &sk) == 0) {
-        std.debug.print("error creating pubkey\n", .{});
-        return;
-    }
-
-    var size: usize = 33;
-    var pk: [33]u8 = undefined;
-    if (c.secp256k1_ec_pubkey_serialize(ctx, &pk, &size, &c_pk, EC_COMPRESSED) == 0) {
-        std.debug.print("error serializing pubkey\n", .{});
-        return;
-    }
-    std.debug.print("public key ({d}): {s}\n", .{ size, std.fmt.fmtSliceHexLower(&pk) });
+    var pk: [32]u8 = undefined;
+    try keys.get_public_key(&pk, &sk);
+    std.debug.print("public key: {s}\n", .{std.fmt.fmtSliceHexLower(&pk)});
 }
