4c53ffaa9f38b96a8aaa09a74201285b4f096155
dymaxion1934 <dymaxion@d.com>
2023-10-06 15:54:48 -0300

    keep a global secp256k1 context.

--- src/keys.zig
+++ src/keys.zig
@@ -6,17 +6,16 @@ const KeyCreationError = error{ ErrorSerializingPublicKey, ErrorCreatingPublicKe
 
 pub fn get_public_key(pk: *[32]u8, sk: *[32]u8) KeyCreationError!void {
     var c_pk: c.secp256k1_pubkey = undefined;
-    var ctx = c.secp256k1_context_create(libsecp256k1.CONTEXT_SIGN);
-    if (c.secp256k1_ec_pubkey_create(ctx, &c_pk, sk) == 0) {
+    if (c.secp256k1_ec_pubkey_create(libsecp256k1.ctx, &c_pk, sk) == 0) {
         return KeyCreationError.ErrorCreatingPublicKey;
     }
 
     var size: usize = 33;
-    var compressed_pk: [33]u8 = undefined;
-    if (c.secp256k1_ec_pubkey_serialize(ctx, &compressed_pk, &size, &c_pk, libsecp256k1.EC_COMPRESSED) == 0) {
+    var comp_pk: [33]u8 = undefined;
+    if (c.secp256k1_ec_pubkey_serialize(libsecp256k1.ctx, &comp_pk, &size, &c_pk, libsecp256k1.EC_COMPRESSED) == 0) {
         return KeyCreationError.ErrorSerializingPublicKey;
     }
 
-    std.mem.copy(u8, pk, compressed_pk[1..33]);
+    std.mem.copy(u8, pk, comp_pk[1..33]);
     return;
 }
