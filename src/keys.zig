b5f7947871d7659ea51a45b674ad0825b4140079
dymaxion1934 <dymaxion@d.com>
2023-10-06 17:37:42 -0300

    ok, turns out we can't create the context at comptime.

--- src/keys.zig
+++ src/keys.zig
@@ -6,13 +6,13 @@ const KeyCreationError = error{ ErrorSerializingPublicKey, ErrorCreatingPublicKe
 
 pub fn get_public_key(pk: *[32]u8, sk: *[32]u8) KeyCreationError!void {
     var c_pk: c.secp256k1_pubkey = undefined;
-    if (c.secp256k1_ec_pubkey_create(libsecp256k1.ctx, &c_pk, sk) == 0) {
+    if (c.secp256k1_ec_pubkey_create(libsecp256k1.getContext(), &c_pk, sk) == 0) {
         return KeyCreationError.ErrorCreatingPublicKey;
     }
 
     var size: usize = 33;
     var comp_pk: [33]u8 = undefined;
-    if (c.secp256k1_ec_pubkey_serialize(libsecp256k1.ctx, &comp_pk, &size, &c_pk, libsecp256k1.EC_COMPRESSED) == 0) {
+    if (c.secp256k1_ec_pubkey_serialize(libsecp256k1.getContext(), &comp_pk, &size, &c_pk, libsecp256k1.EC_COMPRESSED) == 0) {
         return KeyCreationError.ErrorSerializingPublicKey;
     }
 
--- src/libsecp256k1.zig
+++ src/libsecp256k1.zig
@@ -1,3 +1,5 @@
+const c = @cImport(@cInclude("secp256k1.h"));
+
 const FLAGS_TYPE_CONTEXT = 1 << 0;
 const FLAGS_BIT_CONTEXT_VERIFY = 1 << 8;
 const FLAGS_BIT_CONTEXT_SIGN = 1 << 9;
@@ -8,4 +10,13 @@ pub const CONTEXT_VERIFY = (FLAGS_TYPE_CONTEXT | FLAGS_BIT_CONTEXT_VERIFY);
 pub const CONTEXT_SIGN = (FLAGS_TYPE_CONTEXT | FLAGS_BIT_CONTEXT_SIGN);
 pub const EC_COMPRESSED = (FLAGS_TYPE_COMPRESSION | FLAGS_BIT_COMPRESSION);
 
-pub var ctx = c.secp256k1_context_create(CONTEXT_SIGN | CONTEXT_VERIFY);
+pub var ctx: *c.secp256k1_context = undefined;
+var contextCreated = false;
+
+pub fn getContext() *c.secp256k1_context {
+    if (!contextCreated) {
+        ctx = c.secp256k1_context_create(CONTEXT_SIGN | CONTEXT_VERIFY) orelse unreachable;
+        contextCreated = true;
+    }
+    return ctx;
+}
